e939c9337a082ce136cca267015249c36c23e54f
PabloVillagran <pdvillc@gmail.com>
2014-11-10 20:49:25 -0600

    Trabajo del semestre

---
+++ Proyecto fase 2/Pago.java
@@ -0,0 +1,23 @@
+package Interfaz;
+
+public class Pago {
+	int cantidad;
+	String moneda;
+	
+	public Pago(int cantidad, String moneda){
+		this.cantidad = cantidad;
+		this.moneda = moneda;
+	}
+	
+	public void efectivo(){
+		
+	}
+	
+	public void credito(){
+		
+	}
+	
+	public void debito(){
+		
+	}
+}
